74bf0b089ebaae0d78ce395c1b656abf27c9702e
ishiiyuki <mizuki.yuuri@gmail.com>
2021-03-22 00:01:48 +0900

    add Ray clas

---
+++ src/rayt/ray.rs
@@ -0,0 +1,19 @@
+use crate::rayt::*;
+
+#[derive(Debug,Clone,Copy)]
+pub struct Ray {
+    pub origin: Point3,
+    pub direction: Vec3,
+}
+
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        self {origin , direction}
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self
+    }
+}
+
